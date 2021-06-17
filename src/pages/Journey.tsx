b6d737b833f9c80d0611c3e2d08374adab92bcee
lkarasinski <lukasz.karasinski.15@gmail.com>
2021-06-17 23:37:15 +0200

    Added journey selection by link

--- src/pages/Journey.tsx
+++ src/pages/Journey.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
-import JourneyList from 'components/Landing Page/JourneyList';
+import React, { useContext, useEffect, useState } from 'react';
 import Layout from 'components/Layout/Layout';
 import { RouteComponentProps } from 'react-router-dom';
+import firebase from '../firebase';
+import AuthContext from '../contexts/AuthProvider';
 
 interface Props extends RouteComponentProps<{ id: string }> {}
 
 export const Journey: React.FC<Props> = ({ match }) => {
+	const [content, setContent] = useState('');
+	const auth = useContext(AuthContext);
+	const journeysRef = firebase.firestore().collection('journeys');
+
+	useEffect(() => {
+		if (auth.authenticated === true) {
+			journeysRef
+				.doc(match.params.id)
+				.get()
+				.then((doc) => {
+					if (doc.exists) {
+						setContent(doc?.data()?.name);
+					}
+				});
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [auth]);
 	return (
 		<>
 			<Layout>
-				Journeys
-				{match.params.id}
+				<div></div>
+				<h3>Journeys</h3>
+				<pre>CONTENT: {JSON.stringify(content)}</pre>
 			</Layout>
 		</>
 	);
