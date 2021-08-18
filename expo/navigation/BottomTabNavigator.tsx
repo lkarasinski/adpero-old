d1c2d43e79197814b2d580f05eda38aca9ebacb8
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2021-08-18 18:03:55 +0200

    Basic navigation

--- expo/navigation/BottomTabNavigator.tsx
+++ expo/navigation/BottomTabNavigator.tsx
@@ -2,14 +2,20 @@
  * Learn more about createBottomTabNavigator:
  * https://reactnavigation.org/docs/bottom-tab-navigator
  */
-
-import { Ionicons } from '@expo/vector-icons';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import {
+    faColumns,
+    faMapMarkedAlt,
+    faPoll,
+} from '@fortawesome/free-solid-svg-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
+import Polls from '../screens/Polls';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
 import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
@@ -17,34 +23,50 @@ import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
 export default function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <BottomTab.Navigator
-      initialRouteName="TabOne"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
-      <BottomTab.Screen
-        name="TabOne"
-        component={TabOneNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-        }}
-      />
-      <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
+    return (
+        <BottomTab.Navigator
+            initialRouteName="TabOne"
+            tabBarOptions={{
+                activeTintColor: Colors[colorScheme].tint,
+            }}
+        >
+            <BottomTab.Screen
+                name="TabOne"
+                component={TabOneNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <TabBarIcon color={color} icon={faColumns} />
+                    ),
+                }}
+            />
+            <BottomTab.Screen
+                name="TabTwo"
+                component={TabTwoNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <TabBarIcon color={color} icon={faMapMarkedAlt} />
+                    ),
+                }}
+            />
+            <BottomTab.Screen
+                name="Polls"
+                component={PollsNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <TabBarIcon color={color} icon={faPoll} />
+                    ),
+                }}
+            />
+        </BottomTab.Navigator>
+    );
 }
 
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
-function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+function TabBarIcon(props: { icon: IconDefinition; color: string }) {
+    return <FontAwesomeIcon icon={props.icon} color={props.color} size={20} />;
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
@@ -52,27 +74,40 @@ function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']
 const TabOneStack = createStackNavigator<TabOneParamList>();
 
 function TabOneNavigator() {
-  return (
-    <TabOneStack.Navigator>
-      <TabOneStack.Screen
-        name="TabOneScreen"
-        component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
-      />
-    </TabOneStack.Navigator>
-  );
+    return (
+        <TabOneStack.Navigator>
+            <TabOneStack.Screen
+                name="TabOneScreen"
+                component={TabOneScreen}
+                options={{ headerTitle: 'Dashboard' }}
+            />
+        </TabOneStack.Navigator>
+    );
 }
 
 const TabTwoStack = createStackNavigator<TabTwoParamList>();
+const PollsStack = createStackNavigator();
 
 function TabTwoNavigator() {
-  return (
-    <TabTwoStack.Navigator>
-      <TabTwoStack.Screen
-        name="TabTwoScreen"
-        component={TabTwoScreen}
-        options={{ headerTitle: 'Tab Two Title' }}
-      />
-    </TabTwoStack.Navigator>
-  );
+    return (
+        <TabTwoStack.Navigator>
+            <TabTwoStack.Screen
+                name="TabTwoScreen"
+                component={TabTwoScreen}
+                options={{ headerTitle: 'Journeys' }}
+            />
+        </TabTwoStack.Navigator>
+    );
+}
+
+function PollsNavigator() {
+    return (
+        <PollsStack.Navigator>
+            <PollsStack.Screen
+                name="Polls"
+                component={Polls}
+                options={{ headerTitle: 'Polls' }}
+            />
+        </PollsStack.Navigator>
+    );
 }
