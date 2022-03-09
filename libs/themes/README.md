645c0b0f53e6596f35b57ea472a4e75c3a588650
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-03-09 13:51:22 +0100

    feat: :tada: Create themes library

---
+++ libs/themes/README.md
@@ -0,0 +1,11 @@
+# themes
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build themes` to build the library.
+
+## Running unit tests
+
+Run `nx test themes` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/themes/src/lib/default/theme.ts
@@ -0,0 +1,39 @@
+export const theme = {
+  font: {
+    size: {
+      small: '16px',
+      medium: '24px',
+      large: '36px',
+    },
+    weight: {
+      regular: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 700,
+      extraBold: 800,
+      black: 900,
+    },
+    family: 'Nunito',
+  },
+
+  colors: {
+    primary: {
+      regular: '#3d5eff',
+      light: '#738aff',
+    },
+    red: {
+      regular: '#eC6464',
+      light: '#fff2f2',
+    },
+    green: {
+      regular: '#09b54d',
+      light: '#75e6a2',
+    },
+    gray: {
+      dark: '#323232',
+      light: '#676767',
+    },
+    selection: '#c2e2ff',
+    background: '#ffffff',
+  },
+};
