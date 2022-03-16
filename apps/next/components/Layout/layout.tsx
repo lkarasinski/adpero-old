a2c20401d05b477d4ca21f0574f0bd95307e55be
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-03-16 02:40:38 +0100

    feat: :sparkles: Add Layout component

---
+++ apps/next/components/Layout/layout.tsx
@@ -0,0 +1,137 @@
+import React from "react";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import { useEventListener } from "usehooks-ts";
+import Headroom from "react-headroom";
+import Sidebar from "../Sidebar";
+import { useRouter } from "next/router";
+import { useMobile } from "@adpero/hooks";
+import { Text, Burger } from "@adpero/ui";
+import { dashboardTheme } from "@adpero/themes";
+
+interface ContentProps {
+    isMobile: boolean;
+}
+
+export type LayoutProps = {
+    isEditModeEnabled?: boolean;
+};
+
+type RefType = React.MutableRefObject<HTMLDivElement>;
+
+export const Layout: React.FC<LayoutProps> = ({
+    children,
+    isEditModeEnabled,
+}) => {
+    const isMobile = useMobile();
+    const MainRef = React.useRef() as RefType;
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const router = useRouter();
+
+    React.useEffect(() => {
+        if (isMobile) {
+            setIsMenuOpen(false);
+        }
+    }, [router.asPath, isMobile]);
+
+    React.useEffect(() => {
+        setIsMenuOpen(!isMobile);
+    }, [isMobile]);
+
+    const toggleMenu = () => setIsMenuOpen((v) => !v);
+
+    useEventListener(
+        "click",
+        () => {
+            if (isMenuOpen && isMobile) {
+                setIsMenuOpen(false);
+            }
+        },
+        MainRef
+    );
+
+    return (
+        <>
+            <GlobalStyles />
+            <ThemeProvider theme={dashboardTheme}>
+                <StyledMain ref={MainRef}>
+                    <Sidebar
+                        isEditModeEnabled={!!isEditModeEnabled}
+                        isMobile={isMobile}
+                        isMenuOpen={isMenuOpen}
+                        toggleMenu={toggleMenu}
+                    />
+                    <Content isMobile={isMobile}>
+                        {isMobile ? (
+                            <Headroom
+                                style={{
+                                    width: "100vw",
+                                }}
+                            >
+                                <BurgerContainer
+                                    onClick={toggleMenu}
+                                    tabIndex={-1}
+                                    role="button"
+                                    onKeyDown={toggleMenu}
+                                >
+                                    <Burger />
+                                    <Text color="lightPrimary">Adpero</Text>
+                                </BurgerContainer>
+                            </Headroom>
+                        ) : null}
+                        <MobileMargin isMobile={isMobile}>
+                            {children}
+                        </MobileMargin>
+                    </Content>
+                </StyledMain>
+            </ThemeProvider>
+        </>
+    );
+};
+const GlobalStyles = createGlobalStyle`
+    html,
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: Nunito, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    * {
+        box-sizing: border-box;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+}`;
+
+const BurgerContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0rem;
+    padding: 0.25rem 0;
+    background: ${({ theme }) => theme.colors.background};
+    box-shadow: 0 0.25rem 0.5rem 0 rgba(0, 0, 0, 0.0625);
+`;
+
+const MobileMargin = styled.div<ContentProps>`
+    margin: ${({ isMobile }) => (isMobile ? "0rem 1rem" : "0")};
+`;
+
+const Content = styled.div<ContentProps>`
+    z-index: 0;
+    width: ${({ isMobile }) =>
+        isMobile ? `calc(100%) - 1rem` : `calc(100%) - 16rem`};
+    min-height: 100vh;
+    margin: ${({ isMobile }) => (isMobile ? "0" : "0 0 4rem 16rem")};
+    background-color: white;
+    transition: margin-left 100ms ease-in-out;
+`;
+
+const StyledMain = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
