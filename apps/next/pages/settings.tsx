650df5c2d05e4fcda1a31f6dfab336016670d9c2
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-03-23 17:48:06 +0100

    feat: :sparkles: Add login button to settings page

--- apps/next/pages/settings.tsx
+++ apps/next/pages/settings.tsx
@@ -1,8 +1,19 @@
 import * as React from "react";
 import type { NextPage } from "next";
+import { Heading, Label } from "@adpero/ui";
+import LogInButton from "../components/LogInButton";
+import { useAuth } from "@adpero/contexts";
 
 const Settings: NextPage = () => {
-    return <div></div>;
+    const { user } = useAuth();
+
+    return (
+        <div>
+            <Heading>Settings</Heading>
+            <Label>You are {user?.email ? "" : "not"} logged in</Label>
+            <LogInButton />
+        </div>
+    );
 };
 
 export default Settings;
