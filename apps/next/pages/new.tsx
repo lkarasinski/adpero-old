8461892bc129a7c2f833dbb579bcbcedb123deac
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-03-23 17:44:58 +0100

    feat: :sparkles: Add Form to new journey page

--- apps/next/pages/new.tsx
+++ apps/next/pages/new.tsx
@@ -1,8 +1,22 @@
 import * as React from "react";
+import styled from "styled-components";
 import type { NextPage } from "next";
+import { Heading } from "@adpero/ui";
+import EditJourneyDataPanel from "../components/EditJourneyDataPanel";
 
 const NewJourney: NextPage = () => {
-    return <div>New journey</div>;
+    return (
+        <div>
+            <Heading>New journey</Heading>
+            <EditPanelContainer>
+                <EditJourneyDataPanel buttonText="Create new journey" />
+            </EditPanelContainer>
+        </div>
+    );
 };
+
+const EditPanelContainer = styled.div`
+    max-width: 36rem;
+`;
 
 export default NewJourney;
