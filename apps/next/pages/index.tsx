cffc365c482cf2466a45d0155d25bf8375b21775
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-03-23 17:42:47 +0100

    feat: :sparkles: Add Dashboard to index page

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,14 +1,49 @@
-import styled from "styled-components";
-import EditJourneyDataPanel from "../components/EditJourneyDataPanel";
+import * as React from "react";
+import type { NextPage } from "next";
+import { useJourneys } from "@adpero/contexts";
+import { useNotVotedPolls } from "@adpero/hooks";
+import { useMobile } from "@adpero/hooks";
+import { categorizeJourneyData } from "@adpero/functions";
+import { Heading, CardsPanel } from "@adpero/ui";
 
-const StyledPage = styled.div``;
+export const Index: NextPage = () => {
+    const isMobile = useMobile();
+    const { journeys } = useJourneys();
+    const polls = useNotVotedPolls();
+    const journeyData = journeys.map((j) => j.data);
+    const { currentJourneys, upcomingJourneys, futureJourneys, pastJourneys } =
+        categorizeJourneyData(journeyData);
 
-export function Index() {
     return (
-        <StyledPage>
-            <EditJourneyDataPanel buttonText="Witam" />
-        </StyledPage>
+        <div>
+            <Heading>Dashboard</Heading>
+            <CardsPanel
+                isMobile={isMobile}
+                label="Current journeys"
+                cards={currentJourneys}
+            />
+            <CardsPanel
+                isMobile={isMobile}
+                label="Polls you haven't voted in yet"
+                cards={polls}
+            />
+            <CardsPanel
+                isMobile={isMobile}
+                label="Upcoming journeys"
+                cards={upcomingJourneys}
+            />
+            <CardsPanel
+                isMobile={isMobile}
+                label="Future journeys"
+                cards={futureJourneys}
+            />
+            <CardsPanel
+                isMobile={isMobile}
+                label="Past journeys"
+                cards={pastJourneys}
+            />
+        </div>
     );
-}
+};
 
 export default Index;
