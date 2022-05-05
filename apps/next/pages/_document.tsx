05ec96fcdb31409e69db69d29ee97303f567f431
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-05-05 13:05:57 +0200

    feat: :lipstick: Add Nunito font

--- apps/next/pages/_document.tsx
+++ apps/next/pages/_document.tsx
@@ -21,7 +21,18 @@ export default class CustomDocument extends Document<{
     render() {
         return (
             <Html>
-                <Head>{this.props.styleTags}</Head>
+                <Head>
+                    {this.props.styleTags}
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800;900&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
